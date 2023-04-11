248a9db0d106d970649a3ce91f98c124854f6756
martinstajnko <stajnko.martin93@gmail.com>
2023-04-11 20:59:56 +0200

    initial commit

---
+++ holidaytracker/core/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'core/index.html')
+
+
+def dashboard(request):
+    return render(request, 'core/dashboard.html')
